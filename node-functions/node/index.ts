56a7b71fb4ea3da14cc7b6c9c7f0dee340b6e710
Nattsu39 <nattsu39@outlook.com>
2025-11-13 21:10:35 +0800

    测试304

--- node-functions/node/index.ts
+++ node-functions/node/index.ts
@@ -1,12 +1,6 @@
 export function onRequestGet(context) {
 
-  return new Response(JSON.stringify({
-    headers: Object.fromEntries(context.request.headers.entries()),
-    get_test: context.request.headers.get('accept'),
-  }), {
-    headers: {
-      'content-type': 'application/json',
-      'x-edgefunctions': 'Welcome to use EdgeOne Pages Functions.',
-    },
+  return new Response(null, {
+    status: 304,
   });
 }
