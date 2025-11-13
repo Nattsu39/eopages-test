1a1f10d1ccb0db70f285fe21fadf034814108152
Nattsu39 <nattsu39@outlook.com>
2025-11-13 15:20:21 +0800

    update

--- edge-functions/edge/index.ts
+++ edge-functions/edge/index.ts
@@ -1,5 +1,5 @@
 export function onRequestGet(context) {
-  return new Response(JSON.stringify(context.request.headers), {
+  return new Response(JSON.stringify(Object.fromEntries(context.request.headers.entries())), {
     headers: {
       'content-type': 'application/json',
       'x-edgefunctions': 'Welcome to use EdgeOne Pages Functions.',
