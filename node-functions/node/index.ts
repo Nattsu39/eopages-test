3707b2721c3214600ce059930f3ffc629201282d
Nattsu39 <nattsu39@outlook.com>
2025-11-14 12:29:19 +0800

    测试5

--- node-functions/node/index.ts
+++ node-functions/node/index.ts
@@ -1,25 +1,30 @@
-import { getEtagFromRequest } from "./_common";
-
+/**
+ * 从请求头中提取 ETag
+ */
+export function getEtagFromRequest(request): string {
+  return request.headers.get('if-none-match') || request.headers.get('If-None-Match') || '';
+}
 
 export async function onRequestGet(context) {
   const etag = getEtagFromRequest(context.request);
-  if (!etag) {
-    return new Response(JSON.stringify({ test: 'ok' }), {
-      status: 200,
+  if (etag) {
+    return new Response(null, {
+      status: 304,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
         'Cache-Control': 'no-cache',
-        'ETag': '1234567890',
-      },
+      }
     });
   }
-  return new Response(null, {
-    status: 304,
+  return new Response(JSON.stringify({ test: 'ok' }), {
+    status: 200,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Cache-Control': 'no-cache',
-    },
-  });
+      'ETag': '1234567890',
+    }
+  }
+  );
 }
