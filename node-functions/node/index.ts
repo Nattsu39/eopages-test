4621539274514889d0e7673a001788bbdd4523ef
Nattsu39 <nattsu39@outlook.com>
2025-11-14 12:17:27 +0800

    测试3

--- node-functions/node/index.ts
+++ node-functions/node/index.ts
@@ -1,19 +1,18 @@
 import { getEtagFromRequest } from "./_common";
 
 
-export function create200Response(body: any): Response {
-  return new Response(JSON.stringify(body), {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Cache-Control': 'no-cache',
-      'ETag': '1234567890',
-    },
-  });
-}
-
-
-export function create304Response(): Response {
+export async function onRequestGet(context) {
+  const etag = getEtagFromRequest(context.request);
+  if (!etag) {
+    return new Response(JSON.stringify({ test: 'ok' }), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Cache-Control': 'no-cache',
+        'ETag': '1234567890',
+      },
+    });
+  }
   return new Response(null, {
     status: 304,
     headers: {
@@ -22,12 +21,4 @@ export function create304Response(): Response {
       'Cache-Control': 'no-cache',
     },
   });
-}
-
-export async function onRequestGet(context) {
-  const etag = getEtagFromRequest(context.request);
-  if (!etag) {
-    return create200Response({ test: 'ok' });
-  }
-  return create304Response();
 }
